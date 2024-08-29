1d63945a85e0fa7077e7fbffa93e957b23d5564d
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-08-29 19:45:09 +0530

    Add files via upload
    creation ,declaration and intialisation of an array and how to use with the control statements.

---
+++ basic.js
@@ -0,0 +1,27 @@
+//Adding new element
+
+//let num =[1,4,5,7];
+// console.log(num);
+// num[4]=8;
+// console.log(num);
+
+//Insertion
+// Three ways to insert 
+//1- End
+// num.push(9);
+// console.log(num);
+// 2-Begin
+// num.upshift(6);
+// console.log(num);
+// 3-Middle
+// num.splice(10);
+// console.log(num);
+
+
+//how to loop an array
+var arr=[1,2,3,4,5];
+
+arr.forEach(function(val){
+    console.log(val);
+});
+
