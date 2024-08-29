03e1276bb3250f5d462c24d0975117906a8cc1db
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-08-29 19:46:30 +0530

    Add files via upload
    how function works in js , types of function

---
+++ definition.js
@@ -0,0 +1,46 @@
+//function 
+//es5 and es6
+//es6 has three types--->function statement,function expression,anonymous function
+function abcd() {
+    //function statement
+};
+
+var abcd=function() {
+    //function expression
+};
+
+fucntion () {
+    //anonymous function
+};
+
+//es6 only has one that further divides into three--->fat arrow function
+var abcd=()=> {
+    //fat arrow function
+};
+
+//>basic fat arrow, fat arrow with one param,fat arrow with implicit return
+
+var abcd=()=> {
+    //basic fat arrow function
+};
+
+var abcd=param => {
+    //fat arrow with one param
+    //in one param fucntion we can remove fat
+};
+
+var abcd=()=>34;
+//fat arrow with implicit return
+
+
+//JS me essa function jo kuch ni return krta h wo undefined return krta h
+
+//Undefined is a value-->In this case we declared variable but don;t give any value to it.
+
+//not defined is an error-->In this case we don't declared any variable but try to access then it throws an error and that error is called not defined.
+
+//Null is also a value--> In this case we try to find something but it doesn't exist then it gives null.
+//Null is recieved when something is not found
+
+
+
