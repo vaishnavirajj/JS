53ee887a585e5c26b0f1fea0918ecbe9c4705959
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-09-10 23:36:16 +0530

    Update definition.js

--- definition.js
+++ definition.js
@@ -44,3 +44,119 @@ var abcd=()=>34;
 
 
 
+function countVowel(str) {
+    let count=0;
+    for (let i=0;i<str.length;i++){
+        if(str[i] === 'a' || str[i]==='e' || str[i]==='i' || str[i]==='o' || str[i]==='u') {
+            count++;
+        }
+        
+      
+
+}
+return count;
+}
+
+
+
+function countVowel(str)=>{
+    let count=0;
+    for (let i=0;i<str.length;i++){
+        if(str[i] === 'a' || str[i]==='e' || str[i]==='i' || str[i]==='o' || str[i]==='u') {
+            count++;
+        }
+        
+      
+
+}
+return count;
+}
+ 
+
+
+
+var countVowel=(str)=> {
+    let count=0;
+    for (let i=0;i<str.length;i++){
+        if(str[i] === 'a' || str[i]==='e' || str[i]==='i' || str[i]==='o' || str[i]==='u') {
+            count++;
+        }
+        
+      
+
+}
+return count;
+   
+};
+
+
+
+forEach loop in array
+
+let arr=[1,2,3,4,5];
+
+arr.forEach((val)=>{
+    console.log(val);
+
+}
+
+)
+
+//Higher order function /Higher order methods --->passes funnction as parameter or return function
+
+let arr=[2,4,5,7,9,65];
+arr.forEach((val=> {
+    console.log(val*val);
+}))
+
+Same as forEach but it can also returns new array
+let arr=[2,4,5,7,9,65];
+let newArr=arr.map((val=> {
+    return val;
+}))
+console.log(newArr);
+
+
+filter
+
+let arr=[2,4,3,5,7,9];
+let evenArr=arr.filter((val)=>{
+    return val%2===0;
+
+})
+console.log(evenArr);
+
+const output=arr.reduce((res,curr)=> {
+    return res+curr;
+})
+console.log(output);
+
+
+let marks=[1,90,98,78,96,95,45];
+
+let goodMarks=marks.filter((val)=> {
+    return val>90;
+})
+
+console.log(goodMarks);
+
+
+let n=prompt("enter number");
+let arr=[];
+for(let i=1;i<=n;i++) {
+    arr.push(i);
+
+}
+
+let sum=arr.reduce((res,curr)=> {
+    return res+curr;
+})
+console.log(sum);
+
+let prod=arr.reduce((res,curr)=> {
+    return res*curr;
+})
+
+console.log(prod);
+
+
